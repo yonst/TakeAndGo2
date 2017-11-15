f0f38638ad666d79529f1f8095af6ffcecdec90f
yonst <yoni1stern@gmail.com>
2017-11-15 18:05:22 +0200

    Initial commit

---
+++ app/src/main/java/com/car/yona/takeandgo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.car.yona.takeandgo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.car.yona.takeandgo.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
